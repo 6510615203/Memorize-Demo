1639b78bbb8b5c870b0717b30c06becba466061b
Ponthipa <6510615203@student.tu.ac.th>
2025-02-19 13:54:17 +0700

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by นางสาวพรทิพย์พา ธีระประเวศ on 19/2/2568 BE.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
